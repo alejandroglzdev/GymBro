eb69ecc655a9f1adc68195a00d8268e87d61ca39
alejandrogonzalez <gonzalez.espejo.alejandro@alumnat.copernic.cat>
2022-10-18 17:18:35 +0200

    Arrow back implementada tambien en el login, eliminada el arrow que habia hecho adri, y implementado el you alredy have an account

--- app/src/main/java/com/example/gymbro/SignUpActivity.kt
+++ app/src/main/java/com/example/gymbro/SignUpActivity.kt
@@ -1,11 +1,33 @@
 package com.example.gymbro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gymbro.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configureUI()
+    }
+
+    private fun configureUI() {
+        binding.arrowBackImageView.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+
+        }
+
+        binding.signInTextButton.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+
+        }
     }
 }
