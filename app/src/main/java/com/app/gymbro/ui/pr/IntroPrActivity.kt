f1ac865c6e05bc0ff862149e9d75bc2c909f7e8f
adria.fernandez <adria.fernandez@col.vueling.com>
2023-01-22 10:48:51 +0100

    [feat] Implemented datepicker

--- app/src/main/java/com/app/gymbro/ui/pr/IntroPrActivity.kt
+++ app/src/main/java/com/app/gymbro/ui/pr/IntroPrActivity.kt
@@ -1,14 +1,21 @@
 package com.app.gymbro.ui.pr
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import androidx.annotation.RequiresApi
 import com.app.gymbro.databinding.ActivityIntroPrBinding
 import com.app.gymbro.ui.main.MainActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.selects.select
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * This class represents the IntroPrActivity in the app.
@@ -33,6 +40,7 @@ class IntroPrActivity : AppCompatActivity() {
          *
          * @param savedInstanceState: If the activity is being re-initialized after previously being shut down then this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle).
          */
+        @RequiresApi(Build.VERSION_CODES.N)
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             binding = ActivityIntroPrBinding.inflate(layoutInflater)
@@ -44,9 +52,26 @@ class IntroPrActivity : AppCompatActivity() {
         /**
          * Initializes the views for the activity by setting onClickListeners for the back and send button.
          */
+        @RequiresApi(Build.VERSION_CODES.N)
         private fun initViews() {
+
+            var selectedDate: Calendar = Calendar.getInstance()
+
+            val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                selectedDate.set(year, month, dayOfMonth)
+                binding.dateEditTextSignIn.setText(SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(selectedDate.time))
+            }
+
             binding.backImageView.setOnClickListener{ goBackPr() }
             binding.sendButtonPR.setOnClickListener{ addNewPrData() }
+            binding.dateEditTextSignIn.setOnClickListener{
+                binding.dateEditTextSignIn.requestFocus()
+                DatePickerDialog(this, dateSetListener,
+                selectedDate.get(Calendar.YEAR), selectedDate.get(Calendar.MONTH),
+                selectedDate.get(Calendar.DAY_OF_MONTH)).show()
+            }
+
+
         }
 
         /**
